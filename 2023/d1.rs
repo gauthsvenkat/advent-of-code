4bd9bdbf9cbe84f52408cc230e4017dcb0946c4b
Gautham Venkataraman <gautham@dexterenergy.nl>
2023-12-01 08:43:39 +0100

    partial

---
+++ 2023/d1.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main() {
+    let replacements = (
+        ("one", "o1e"),
+        ("two", "t2o"),
+        ("three", "t3e"),
+        ("four", "4"),
+        ("five", "5e"),
+        ("six", "6"),
+        ("seven", "7n"),
+        ("eight", "e8t"),
+        ("nine", "n9e"),
+    );
+
+    for line in io::stdin().lines() {
+        let line = line.unwrap();
+    }
+}
