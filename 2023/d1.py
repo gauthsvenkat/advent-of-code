b987bdeba63d9115cd3cd7a1776c1505ef27a265
Gautham Venkataraman <gautham@dexterenergy.nl>
2023-12-01 07:13:59 +0100

    d1 done

--- 2023/d1.py
+++ 2023/d1.py
@@ -1,15 +1,24 @@
 import sys
 
 agg = 0
+
 for line in sys.stdin:
-    line = line.strip().replace("one", "1").replace("two", "2").replace("three", "3").replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7").replace("eight", "8").replace("nine", "9")
+    for r in (
+        ("one", "o1e"),
+        ("two", "t2o"),
+        ("three", "t3e"),
+        ("four", "4"),
+        ("five", "5e"),
+        ("six", "6"),
+        ("seven", "7n"),
+        ("eight", "e8t"),
+        ("nine", "n9e"),
+    ):
+        line = line.replace(*r)
 
-    nums = ''.join(c for c in line if c.isdigit())
-    
-    if nums:
-        num = nums[0] + nums[-1]
-        agg += int(num)
+    if not (nums := "".join(c for c in line if c.isdigit())):
+        continue
 
-        print(f"{num=} {agg=}")
+    agg += int(nums[0] + nums[-1])
 
 print(agg)
