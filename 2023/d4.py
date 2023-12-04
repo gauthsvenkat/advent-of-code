f010cd580a0d290961764ae275a639640476ef64
Gautham Venkataraman <gautham@dexterenergy.nl>
2023-12-04 09:56:33 +0100

    part 2 done

--- 2023/d4.py
+++ 2023/d4.py
@@ -1,10 +1,9 @@
 import sys
 
-agg = 0
+input = [line.strip() for line in sys.stdin if line != "\n"]
 
-for line in [line for line in sys.stdin if line != "\n"]:
-    line = line.strip()
 
+def parse(line):
     card_number, numbers = line.split(": ")
     card_number = int(card_number.replace("Card ", ""))
 
@@ -12,10 +11,21 @@ for line in [line for line in sys.stdin if line != "\n"]:
     winning_numbers = {int(n) for n in winning_numbers.split(" ") if n != ""}
     our_numbers = {int(n) for n in our_numbers.split(" ") if n != ""}
 
+    return card_number, (winning_numbers, our_numbers)
+
+
+agg = 0
+scratchcard_count = {i + 1: 1 for i in range(len(input))}
+
+for line in input:
+    card_number, (winning_numbers, our_numbers) = parse(line)
     common_numbers = winning_numbers & our_numbers
-
     num_common = len(common_numbers)
+    agg += int(2 ** (num_common - 1)) if num_common else 0
 
-    agg += int(2 ** (num_common-1)) if num_common else 0
+    for r in range(scratchcard_count[card_number]):
+        for i in range(num_common):
+            scratchcard_count[card_number + i + 1] += 1
 
 print(agg)
+print(sum(scratchcard_count.values()))
