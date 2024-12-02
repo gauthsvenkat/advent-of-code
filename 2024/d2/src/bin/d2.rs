b5249f39e751707ea1841e3ddfbd8fff83c8d074
Gautham Venkataraman <gautham@dexterenergy.ai>
2024-12-02 08:46:47 +0100

    WIP
    attempt p2
    
    got p2

---
+++ 2024/d2/src/bin/d2.rs
@@ -0,0 +1,96 @@
+use std::env;
+use std::fs;
+
+fn get_input() -> String {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    fs::read_to_string(file_path).unwrap()
+}
+
+fn parse(input: &str) -> Vec<Vec<i32>> {
+    let mut reports: Vec<Vec<i32>> = Vec::new();
+
+    for line in input.lines() {
+        let mut levels: Vec<i32> = Vec::new();
+        for num in line.split_whitespace() {
+            levels.push(num.parse().unwrap());
+        }
+        reports.push(levels);
+    }
+
+    reports
+}
+
+fn is_safe(report: &Vec<i32>) -> bool {
+    let mut p_delta: i32 = 0;
+
+    for w in report.windows(2) {
+        let c_delta: i32 = w[0] - w[1];
+        let c_delta_abs = c_delta.abs();
+
+        if (c_delta_abs < 1) || (c_delta_abs > 3) || ((c_delta * p_delta) < 0) {
+            return false;
+        }
+
+        p_delta = c_delta;
+    }
+
+    true
+}
+
+fn p1(input: &str) -> u32 {
+    let reports = parse(input);
+
+    let mut num_safe: u32 = 0;
+
+    for report in &reports {
+        if is_safe(&report) {
+            num_safe += 1;
+        }
+    }
+
+    num_safe
+}
+
+fn combinations_excluding_one(input: &Vec<i32>) -> Vec<Vec<i32>> {
+    let mut combinations = Vec::new();
+
+    for i in 0..input.len() {
+        let mut copy = input.clone();
+        copy.remove(i);
+        combinations.push(copy);
+    }
+
+    combinations
+}
+
+fn p2(input: &str) -> u32 {
+    let reports = parse(input);
+
+    let mut num_safe: u32 = 0;
+
+    for report in &reports {
+        if is_safe(&report) {
+            num_safe += 1;
+        } else {
+            for combi in combinations_excluding_one(&report) {
+                if is_safe(&combi) {
+                    num_safe += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    num_safe
+}
+
+fn main() {
+    let input = get_input();
+
+    let p1_sol = p1(&input);
+    println!("{p1_sol}");
+
+    let p2_sol = p2(&input);
+    println!("{p2_sol}");
+}
