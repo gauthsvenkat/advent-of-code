95962bc354ef266e0555fb00cf9bd96c436a0c5d
Gautham Venkataraman <gautham@dexterenergy.ai>
2024-12-09 13:10:47 +0100

    ugly as shit but lord knows it works

--- 2024/d9/src/main.rs
+++ 2024/d9/src/main.rs
@@ -36,11 +36,8 @@ fn defragment_blocks(expanded_diskmap: Vec<Option<usize>>) -> Vec<usize> {
 
     let mut defragmented_diskmap = Vec::new();
 
-    for i in 0..expanded_diskmap.len() {
-        if i > right_idx {
-            break;
-        }
-
+    let mut i = 0;
+    while i <= right_idx {
         match expanded_diskmap[i] {
             Some(a) => defragmented_diskmap.push(a),
             None => {
@@ -51,6 +48,7 @@ fn defragment_blocks(expanded_diskmap: Vec<Option<usize>>) -> Vec<usize> {
                 right_idx -= 1;
             }
         }
+        i += 1;
     }
 
     defragmented_diskmap
@@ -68,10 +66,103 @@ fn p1(input: &str) -> usize {
         .sum()
 }
 
+fn defragment_files(mut expanded_diskmap: Vec<Option<usize>>) -> Vec<Option<usize>> {
+    let total_length = expanded_diskmap.len();
+    let mut right_idx = total_length - 1;
+
+    while right_idx > 0 {
+        // println!("\n");
+        // println!("right_idx: {}", right_idx);
+        // expanded_diskmap.iter().for_each(|a| {
+        //     print!(
+        //         "{}",
+        //         match a {
+        //             Some(v) => char::from_digit(*v as u32, 10).unwrap(),
+        //             None => '.',
+        //         }
+        //     );
+        // });
+        // println!("");
+
+        // move to start of non empty space from right
+        while expanded_diskmap[right_idx].is_none() {
+            right_idx -= 1;
+        }
+
+        let file_id = expanded_diskmap[right_idx].unwrap();
+        let mut file_size = 0;
+
+        // println!("right_idx new: {}", right_idx);
+
+        while right_idx > file_size
+            && match expanded_diskmap[right_idx - file_size] {
+                Some(a) => a == file_id,
+                None => false,
+            }
+        {
+            file_size += 1;
+        }
+
+        // println!("file_id: {}, file_size: {}", file_id, file_size);
+
+        let mut left_idx = 0;
+        let mut empty_size = 0;
+
+        while left_idx < right_idx - file_size {
+            // move to the start of an empty space from left
+            while expanded_diskmap[left_idx].is_some() {
+                left_idx += 1;
+            }
+
+            // check from the left for the first empty space
+            // which is >= file_size
+            while expanded_diskmap[left_idx + empty_size].is_none() {
+                empty_size += 1;
+                if left_idx + empty_size >= total_length {
+                    break;
+                }
+            }
+
+            if empty_size >= file_size {
+                break;
+            } else {
+                left_idx += empty_size;
+                empty_size = 0;
+            }
+        }
+
+        // println!("empty_size: {}", empty_size);
+
+        // If found, move the file to the empty space
+        if empty_size >= file_size && left_idx < right_idx {
+            // everything from right_idx to right_idx - file_size should move
+            // to left_idx to left_idx + file_size
+
+            for i in 0..file_size {
+                expanded_diskmap[left_idx + i] = expanded_diskmap[right_idx - i];
+                expanded_diskmap[right_idx - i] = None;
+            }
+        }
+
+        right_idx -= file_size;
+    }
+
+    expanded_diskmap
+}
+
 fn p2(input: &str) -> usize {
-    let parsed_input = parse(input);
-    // TODO:
-    todo!()
+    let diskmap = parse(input);
+    let expanded_diskmap = parse_diskmap(&diskmap);
+    let defragmented_diskmap = defragment_files(expanded_diskmap);
+
+    defragmented_diskmap
+        .iter()
+        .enumerate()
+        .map(|(i, a)| match a {
+            Some(v) => i * v,
+            None => 0,
+        })
+        .sum()
 }
 
 fn main() {
