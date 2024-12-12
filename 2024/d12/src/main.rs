19602470ae604378ee53cadd3cea683ed5e0cfea
Gautham Venkataraman <gautham@dexterenergy.ai>
2024-12-12 10:36:59 +0100

    p1 done

--- 2024/d12/src/main.rs
+++ 2024/d12/src/main.rs
@@ -6,61 +6,98 @@ fn parse(input: &str) -> Vec<Vec<char>> {
     input.lines().map(|l| l.trim().chars().collect()).collect()
 }
 
-fn group_plots(plot: &Vec<Vec<char>>) -> HashMap<char, Vec<(usize, usize)>> {
-    let mut groups: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
-
-    for (i, row) in plot.iter().enumerate() {
-        for (j, cell) in row.iter().enumerate() {
-            groups.entry(*cell).or_default().push((i, j));
-        }
+fn get_contiguous(
+    pos: (usize, usize),
+    plot: &Vec<Vec<char>>,
+    grid_dim: (usize, usize),
+    mut contiguous_plot: HashSet<(usize, usize)>,
+) -> HashSet<(usize, usize)> {
+    if contiguous_plot.contains(&pos) {
+        return contiguous_plot;
     }
-    groups
+
+    contiguous_plot.insert(pos);
+
+    let (i, j) = pos;
+    let (h, w) = grid_dim;
+
+    if j > 0 && plot[i][j - 1] == plot[i][j] {
+        contiguous_plot = get_contiguous((i, j - 1), plot, grid_dim, contiguous_plot);
+    }
+
+    if i < h - 1 && plot[i + 1][j] == plot[i][j] {
+        contiguous_plot = get_contiguous((i + 1, j), plot, grid_dim, contiguous_plot);
+    }
+
+    if i > 0 && plot[i - 1][j] == plot[i][j] {
+        contiguous_plot = get_contiguous((i - 1, j), plot, grid_dim, contiguous_plot);
+    }
+
+    if j < w - 1 && plot[i][j + 1] == plot[i][j] {
+        contiguous_plot = get_contiguous((i, j + 1), plot, grid_dim, contiguous_plot);
+    }
+
+    contiguous_plot
 }
 
-fn group_contiguous(plot: &Vec<Vec<char>>, sub_plot: &Vec<(usize, usize)>) -> Vec<HashSet<(usize,usize)>>
-
-fn count_perimeter(plot: &Vec<Vec<char>>, sub_plot: &Vec<(usize, usize)>) -> usize {
+fn count_perimeter(plot: &Vec<Vec<char>>, contiguous_plot: &HashSet<(usize, usize)>) -> usize {
     let mut perimeter = 0;
 
     let max_i = plot.len() - 1;
     let max_j = plot[0].len() - 1;
 
-    for (i, j) in sub_plot {
+    for (i, j) in contiguous_plot {
         if *j == 0 || plot[*i][j - 1] != plot[*i][*j] {
             perimeter += 1;
-            println!("left");
+            // println!("left");
         }
         if *i == max_i || plot[i + 1][*j] != plot[*i][*j] {
             perimeter += 1;
-            println!("down");
+            // println!("down");
         }
         if *i == 0 || plot[i - 1][*j] != plot[*i][*j] {
             perimeter += 1;
-            println!("up");
+            // println!("up");
         }
         if *j == max_j || plot[*i][j + 1] != plot[*i][*j] {
             perimeter += 1;
-            println!("right");
+            // println!("right");
         }
     }
 
-    println!(
-        "{:?}: {:?} x {:?}={:?}",
-        plot[sub_plot[0].0][sub_plot[0].1],
-        sub_plot,
-        perimeter,
-        sub_plot.len() * perimeter
-    );
+    // println!(
+    //     "{:?}: {:?} x {:?}={:?}",
+    //     plot[sub_plot[0].0][sub_plot[0].1],
+    //     sub_plot,
+    //     perimeter,
+    //     sub_plot.len() * perimeter
+    // );
     perimeter
 }
 
 fn p1(input: &str) -> usize {
     let plot = parse(input);
-    let grouped_plot = group_plots(&plot);
 
-    grouped_plot
-        .values()
-        .map(|sub_plot| count_perimeter(&plot, sub_plot) * sub_plot.len())
+    let grid_dim = (plot.len(), plot[0].len());
+
+    let mut visited_positions = HashSet::new();
+    let mut contiguous_plots: Vec<HashSet<(usize, usize)>> = Vec::new();
+
+    for i in 0..grid_dim.0 {
+        for j in 0..grid_dim.1 {
+            if visited_positions.contains(&(i, j)) {
+                continue;
+            }
+            let contiguous = get_contiguous((i, j), &plot, grid_dim, HashSet::new());
+
+            contiguous_plots.push(contiguous.clone());
+            visited_positions.extend(contiguous);
+        }
+    }
+
+    contiguous_plots
+        .iter()
+        .map(|c| count_perimeter(&plot, c) * c.len())
         .sum()
 }
 
