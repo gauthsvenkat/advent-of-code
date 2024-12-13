19985479e60df12e6a81f7a26ebf93f93ac1f891
Gautham Venkataraman <gautham@dexterenergy.ai>
2024-12-13 14:58:58 +0100

    done with d13

--- 2024/d13/src/main.rs
+++ 2024/d13/src/main.rs
@@ -1,19 +1,18 @@
 use regex::Regex;
-use std::collections::HashSet;
 use std::env;
 use std::fs;
 
-fn parse(input: &str) -> Vec<((i32, i32), (i32, i32), (i32, i32))> {
+fn parse(input: &str) -> Vec<((i64, i64), (i64, i64), (i64, i64))> {
     let re = Regex::new(r"X[+=](\d+), Y[+=](\d+)").unwrap();
 
     input
         .split("\n\n")
         .map(|block| {
-            let tuples: Vec<(i32, i32)> = re
+            let tuples: Vec<(i64, i64)> = re
                 .captures_iter(block)
                 .map(|cap| {
-                    let x = cap[1].parse::<i32>().unwrap();
-                    let y = cap[2].parse::<i32>().unwrap();
+                    let x = cap[1].parse::<i64>().unwrap();
+                    let y = cap[2].parse::<i64>().unwrap();
                     (x, y)
                 })
                 .collect();
@@ -23,77 +22,60 @@ fn parse(input: &str) -> Vec<((i32, i32), (i32, i32), (i32, i32))> {
         .collect()
 }
 
-fn egcd(p: i32, q: i32) -> (i32, i32, i32) {
-    if p == 0 {
-        return (q, 0, 1);
+fn solve(x: (i64, i64, i64), y: (i64, i64, i64)) -> Option<(i64, i64)> {
+    let (a1, b1, c1) = x;
+    let (a2, b2, c2) = y;
+
+    let determinant = a1 * b2 - a2 * b1;
+
+    if determinant == 0 {
+        return None;
     }
 
-    let (gcd, x1, y1) = egcd(q % p, p);
-    let ca = y1 - (q / p) * x1;
-    let cb = x1;
+    let a_numerator = c1 * b2 - c2 * b1;
+    let b_numerator = a1 * c2 - a2 * c1;
 
-    (gcd, ca, cb)
-}
-
-fn find_presses(x: i32, y: i32, t: i32) -> HashSet<(i32, i32)> {
-    let (gcd, ca, cb) = egcd(x, y);
-
-    dbg!(x, y, t, gcd, ca, cb);
-
-    if t % gcd != 0 {
-        return HashSet::new();
+    if a_numerator % determinant == 0 && b_numerator % determinant == 0 {
+        let a = a_numerator / determinant;
+        let b = b_numerator / determinant;
+        Some((a, b))
+    } else {
+        None
     }
-
-    let a0 = ca * (t / gcd);
-    let b0 = cb * (t / gcd);
-
-    /*
-     * min 3a + b where
-     * a = a0 + (y / gcd) * t
-     * b = b0 - (x / gcd) * t
-     * a, b > 0
-     * t is any integer
-     */
-
-    /*
-     * since a > 0 => a0 + (y / gcd) * t > 0
-     * -a0 * gcd / y < t
-     */
-
-    /*
-     * since b > 0 => b0 - (x / gcd) * t > 0
-     * t < b0 * gcd / x
-     */
-
-    /*
-     * therefore (-a0 * gcd / y) < t < (b0 * gcd / x)
-     */
-
-    let t_min: i32 = -a0 * gcd / y;
-    let t_max: i32 = b0 * gcd / x;
-
-    dbg!(a0, b0, t_min, t_max);
-    (t_min..t_max)
-        .map(|t| (a0 + (y / gcd) * t, b0 - (x / gcd) * t))
-        .filter(|(a, b)| a > &0 && b > &0)
-        .collect()
 }
 
-fn p1(input: &str) -> i32 {
+fn p1(input: &str) -> i64 {
     let machines = parse(input);
 
-    machines.iter().map(|(x, y, t)| {
-        find_presses(x.0, y.0, t.0)
-            .intersection(&find_presses(x.1, y.1, t.1))
-            .map(|(a, b)| a * 3 + b)
-            .min()
-            .unwrap_or(0)
-    }).sum()
+    machines
+        .iter()
+        .map(|(button_a, button_b, prize)| {
+            let (a, b) = solve(
+                (button_a.0, button_b.0, prize.0),
+                (button_a.1, button_b.1, prize.1),
+            )
+            .unwrap_or((0, 0));
+
+            3 * a + b
+        })
+        .sum()
 }
 
-fn p2(input: &str) -> usize {
-    let parsed_input = parse(input);
-    todo!()
+fn p2(input: &str) -> i64 {
+    let machines = parse(input);
+
+    machines
+        .iter()
+        .map(|(button_a, button_b, prize)| {
+            let (a, b) = solve(
+                (button_a.0, button_b.0, prize.0 + 10000000000000),
+                (button_a.1, button_b.1, prize.1 + 10000000000000),
+            )
+            .unwrap_or((0, 0));
+
+            3 * a + b
+        })
+        .sum()
 }
 
 fn main() {
