42966422ba76100a7d88c67f0f52a8396e5e8fa3
Gautham Venkataraman <gauthsvenkat@gmail.com>
2024-12-25 10:37:04 +0100

    Done with d21
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP

---
+++ 2024/d21/src/main.rs
@@ -0,0 +1,205 @@
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap};
+use std::env;
+use std::fs;
+use std::iter;
+
+fn parse(input: &str) -> Vec<String> {
+    input.lines().map(|s| s.to_string()).collect()
+}
+
+type Position = (usize, usize);
+
+struct Keypad {
+    keys: HashMap<char, Position>,
+    n_row: usize,
+    n_col: usize,
+    nogo: Position,
+}
+
+impl Keypad {
+    fn numeric() -> Self {
+        /*
+        7 8 9
+        4 5 6
+        1 2 3
+          0 A
+        */
+        Keypad {
+            keys: HashMap::from([
+                ('7', (0, 0)),
+                ('8', (0, 1)),
+                ('9', (0, 2)),
+                ('4', (1, 0)),
+                ('5', (1, 1)),
+                ('6', (1, 2)),
+                ('1', (2, 0)),
+                ('2', (2, 1)),
+                ('3', (2, 2)),
+                // (' ', (3, 0)),
+                ('0', (3, 1)),
+                ('A', (3, 2)),
+            ]),
+            n_row: 4,
+            n_col: 3,
+            nogo: (3, 0),
+        }
+    }
+
+    fn directional() -> Self {
+        /*
+          ^ A
+        < v >
+        */
+        Keypad {
+            keys: HashMap::from([
+                // (' ', (0, 0)),
+                ('^', (0, 1)),
+                ('A', (0, 2)),
+                ('<', (1, 0)),
+                ('v', (1, 1)),
+                ('>', (1, 2)),
+            ]),
+            n_row: 2,
+            n_col: 3,
+            nogo: (0, 0),
+        }
+    }
+}
+
+fn travel(keypad: &Keypad, current_char: char, target_char: char) -> Vec<String> {
+    let (current_position, target_position) =
+        (keypad.keys[&current_char], keypad.keys[&target_char]);
+
+    let mut pq: BinaryHeap<Reverse<(usize, Position, String)>> = BinaryHeap::new();
+    pq.push(Reverse((0, current_position, String::new())));
+
+    let mut best_cost: Option<usize> = None;
+    let mut paths: Vec<String> = Vec::new();
+
+    while let Some(Reverse((cost, position, mut path))) = pq.pop() {
+        let (i, j) = position;
+
+        if position == target_position {
+            if let Some(bc) = best_cost {
+                if cost > bc {
+                    break;
+                }
+            } else {
+                best_cost = Some(cost);
+            }
+
+            paths.push({
+                path.push('A');
+                path
+            });
+
+            continue;
+        }
+
+        for (c, (di, dj)) in [('<', (0, -1)), ('v', (1, 0)), ('^', (-1, 0)), ('>', (0, 1))] {
+            let (ni, nj) = (i as i8 + di, j as i8 + dj);
+
+            if ni < 0 || nj < 0 {
+                continue;
+            }
+
+            let (ni, nj) = (ni as usize, nj as usize);
+
+            if ni >= keypad.n_row || nj >= keypad.n_col || (ni, nj) == keypad.nogo {
+                continue;
+            }
+
+            pq.push(Reverse((cost + 1, (ni, nj), {
+                let mut updated_path = path.clone();
+                updated_path.push(c);
+                updated_path
+            })));
+        }
+    }
+
+    paths
+}
+
+fn button_sequence(
+    code: &str,
+    keypad: &Keypad,
+    cache: &mut HashMap<(char, char, usize), usize>,
+    depth: usize,
+) -> usize {
+    if depth == 0 {
+        return code.len();
+    }
+
+    iter::once('A')
+        .chain(code.chars())
+        .collect::<Vec<char>>()
+        .windows(2)
+        .map(|window| {
+            let (current_char, next_char) = (window[0], window[1]);
+
+            if let Some(&len) = cache.get(&(current_char, next_char, depth)) {
+                len
+            } else {
+                let len = travel(keypad, current_char, next_char)
+                    .iter()
+                    .map(|path| button_sequence(path, &Keypad::directional(), cache, depth - 1))
+                    .min()
+                    .unwrap();
+
+                cache.insert((current_char, next_char, depth), len);
+
+                len
+            }
+        })
+        .sum()
+}
+
+fn get_numeric_part(code: &str) -> usize {
+    code.chars()
+        .filter(|c| c.is_numeric())
+        .collect::<String>()
+        .parse()
+        .unwrap()
+}
+
+fn p1(input: &str) -> usize {
+    let codes = parse(input);
+
+    let mut cache = HashMap::new();
+
+    codes
+        .iter()
+        .map(|code| {
+            button_sequence(code, &Keypad::numeric(), &mut cache, 3) * get_numeric_part(code)
+        })
+        .sum()
+}
+
+fn p2(input: &str) -> usize {
+    let codes = parse(input);
+
+    let mut cache = HashMap::new();
+
+    codes
+        .iter()
+        .map(|code| {
+            button_sequence(code, &Keypad::numeric(), &mut cache, 26) * get_numeric_part(code)
+        })
+        .sum()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let part = &args[1];
+    let filepath = &args[2];
+
+    let input = fs::read_to_string(filepath).unwrap();
+
+    match part.as_str() {
+        "p1" => println!("{}", p1(&input)),
+        "p2" => println!("{}", p2(&input)),
+        _ => panic!("Invalid part"),
+    };
+}
--- 2024/d24/src/main.rs
+++ 2024/d24/src/main.rs
@@ -1,9 +1,9 @@
 use regex::Regex;
-use std::collections::{BTreeMap, VecDeque};
+use std::collections::{BTreeMap, BTreeSet, VecDeque};
 use std::env;
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Gate {
     And(String, String),
     Xor(String, String),
@@ -90,21 +90,162 @@ fn eval_gates(
     realized
 }
 
-fn p1(input: &str) -> usize {
-    let (realized, connections) = parse(input);
-    let realized = eval_gates(realized, connections);
-
+fn gate_to_number(realized: &BTreeMap<String, bool>, gate: &str) -> usize {
     realized
         .iter()
-        .filter_map(|(k, &v)| if k.starts_with('z') { Some(v) } else { None })
+        .filter_map(|(k, &v)| if k.starts_with(gate) { Some(v) } else { None })
         .enumerate()
         .filter_map(|(i, bit)| if bit { Some(1 << i) } else { None })
         .sum()
 }
 
+fn p1(input: &str) -> usize {
+    let (realized, connections) = parse(input);
+    let realized = eval_gates(realized, connections);
+
+    gate_to_number(&realized, "z")
+}
+
+fn check_output(realized: &BTreeMap<String, bool>) -> bool {
+    let x = gate_to_number(realized, "x");
+    let y = gate_to_number(realized, "y");
+    let z = gate_to_number(realized, "z");
+
+    x + y == z
+}
+
+fn new_realized() -> BTreeMap<String, bool> {
+    let mut realized = BTreeMap::new();
+    for i in 0..45 {
+        realized.insert(format!("x{:02}", i), false);
+        realized.insert(format!("y{:02}", i), false);
+    }
+    realized
+}
+
+fn get_sus(connections: BTreeMap<String, Gate>) -> BTreeSet<(String, String)> {
+    let mut sus = BTreeSet::new();
+
+    for i in 0..45 {
+        // both x and y are 0
+        let mut realized = new_realized();
+        realized.insert(format!("x{:02}", i), false);
+        realized.insert(format!("y{:02}", i), false);
+        if !check_output(&eval_gates(realized, connections.clone())) {
+            if let Some(gate) = connections.get(&format!("z{:02}", i)) {
+                match gate {
+                    Gate::And(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                    Gate::Or(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                    Gate::Xor(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                }
+            }
+        }
+
+        // x is 0, y is 1
+        let mut realized = new_realized();
+        realized.insert(format!("x{:02}", i), false);
+        realized.insert(format!("y{:02}", i), true);
+        if !check_output(&eval_gates(realized, connections.clone())) {
+            if let Some(gate) = connections.get(&format!("z{:02}", i)) {
+                match gate {
+                    Gate::And(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                    Gate::Or(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                    Gate::Xor(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                }
+            }
+        }
+
+        // x is 1, y is 0
+        let mut realized = new_realized();
+        realized.insert(format!("x{:02}", i), true);
+        realized.insert(format!("y{:02}", i), false);
+        if !check_output(&eval_gates(realized, connections.clone())) {
+            if let Some(gate) = connections.get(&format!("z{:02}", i)) {
+                match gate {
+                    Gate::And(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                    Gate::Or(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                    Gate::Xor(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                }
+            }
+        }
+
+        // x is 1, y is 1
+        let mut realized = new_realized();
+        realized.insert(format!("x{:02}", i), true);
+        realized.insert(format!("y{:02}", i), true);
+        if !check_output(&eval_gates(realized, connections.clone())) {
+            if let Some(gate) = connections.get(&format!("z{:02}", i)) {
+                match gate {
+                    Gate::And(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                    Gate::Or(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                    Gate::Xor(a, b) => {
+                        sus.insert((a.to_string(), b.to_string()));
+                    }
+                }
+            }
+        }
+    }
+
+    sus.into_iter()
+        .filter(|k| !k.0.starts_with('x') && !k.0.starts_with('y'))
+        .collect()
+}
+
 fn p2(input: &str) -> usize {
-    let parsed_input = parse(input);
-    todo!()
+    let (realized, connections) = parse(input);
+    // let realized = eval_gates(realized, connections);
+
+    // let x: usize = realized
+    //     .iter()
+    //     .filter_map(|(k, &v)| if k.starts_with('x') { Some(v) } else { None })
+    //     .enumerate()
+    //     .filter_map(|(i, bit)| if bit { Some(1 << i) } else { None })
+    //     .sum();
+    //
+    // let y: usize = realized
+    //     .iter()
+    //     .filter_map(|(k, &v)| if k.starts_with('y') { Some(v) } else { None })
+    //     .enumerate()
+    //     .filter_map(|(i, bit)| if bit { Some(1 << i) } else { None })
+    //     .sum();
+    //
+    // let z: usize = realized
+    //     .iter()
+    //     .filter_map(|(k, &v)| if k.starts_with('z') { Some(v) } else { None })
+    //     .enumerate()
+    //     .filter_map(|(i, bit)| if bit { Some(1 << i) } else { None })
+    //     .sum();
+    //
+    // println!("x: {} {:b}", x, x);
+    // println!("y: {} {:b}", y, y);
+    // println!("i z: {} {:b}", z, z);
+    // println!("c z: {} {:b}", x + y, x + y);
+
+    let sus = get_sus(connections);
+    dbg!(&sus);
+    0
 }
 
 fn main() {
