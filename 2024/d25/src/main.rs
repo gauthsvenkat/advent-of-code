75221488e4f358f4c2f5ba7f97f030b12f330f9a
Gautham Venkataraman <gauthsvenkat@gmail.com>
2024-12-25 08:19:07 +0100

    done with p1

---
+++ 2024/d25/src/main.rs
@@ -0,0 +1,71 @@
+use std::env;
+use std::fs;
+
+fn parse(input: &str) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
+    let mut locks: Vec<Vec<u8>> = Vec::new();
+    let mut keys: Vec<Vec<u8>> = Vec::new();
+
+    input.split("\n\n").for_each(|b| {
+        let block = b.split("\n").collect::<Vec<&str>>();
+
+        let mut col_heights: Vec<u8> = Vec::new();
+
+        let num_columns = block[0].len();
+
+        for j in 0..num_columns {
+            let mut col_height = 0;
+            for row in &block {
+                if let Some(c) = row.chars().nth(j) {
+                    if c == '#' {
+                        col_height += 1;
+                    }
+                }
+            }
+            col_heights.push(col_height - 1);
+        }
+
+        if block[0].starts_with("#####") {
+            locks.push(col_heights);
+        } else {
+            keys.push(col_heights);
+        }
+    });
+
+    (locks, keys)
+}
+
+fn p1(input: &str) -> usize {
+    let (locks, keys) = parse(input);
+
+    let mut fit_count = 0;
+
+    for lock in locks.iter() {
+        for key in keys.iter() {
+            if lock.iter().zip(key).all(|(l, k)| l + k < 6) {
+                fit_count += 1;
+            }
+        }
+    }
+
+    fit_count
+}
+
+fn p2(input: &str) -> usize {
+    let parsed_input = parse(input);
+    todo!()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let part = &args[1];
+    let filepath = &args[2];
+
+    let input = fs::read_to_string(filepath).unwrap();
+
+    match part.as_str() {
+        "p1" => println!("{}", p1(&input)),
+        "p2" => println!("{}", p2(&input)),
+        _ => panic!("Invalid part"),
+    };
+}
