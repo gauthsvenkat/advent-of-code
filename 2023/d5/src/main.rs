c6977667b6c70e44ab9268244c1b95812d6950ea
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-01-13 20:39:54 +0530

    WIP

--- 2023/d5/src/main.rs
+++ 2023/d5/src/main.rs
@@ -4,6 +4,8 @@ use std::fs;
 
 type Map = Vec<(usize, usize, usize)>;
 type Maps = HashMap<String, Map>;
+type Range = (usize, usize);
+type Ranges = Vec<Range>;
 
 fn parse(input: &str) -> (Vec<usize>, Maps) {
     let blocks: Vec<&str> = input.split("\n\n").collect();
@@ -54,6 +56,29 @@ fn get(maps: &Maps, map_type: &str, n: usize) -> usize {
     n
 }
 
+fn mapper(maps: &Maps, map_type: &str, mut inputs: Ranges, mut outputs: Ranges) -> Ranges {
+    while let Some((is, ir)) = inputs.pop() {
+        let ie = is + (ir - 1);
+
+        for (od, os, or) in maps.get(map_type).unwrap() {
+            let oe = os + (or - 1);
+            // If the input range is contained
+            // in the output range
+            if is >= *os && ie < oe {
+                outputs.push((is - os + od, *or))
+            } else if is < *os && ie < oe {
+                todo!()
+            } else if is >= *os && ie >= oe {
+                todo!()
+            } else {
+                todo!()
+            }
+        }
+    }
+
+    outputs
+}
+
 fn p1(input: &str) -> usize {
     let (seeds, maps) = parse(input);
 
@@ -71,8 +96,20 @@ fn p1(input: &str) -> usize {
 }
 
 fn p2(input: &str) -> usize {
-    let parsed_input = parse(input);
-    todo!()
+    let (seeds, maps) = parse(input);
+
+    seeds
+        .chunks(2)
+        .flat_map(|s| s[0]..s[0] + s[1])
+        .map(|n| get(&maps, "seed-to-soil", n))
+        .map(|n| get(&maps, "soil-to-fertilizer", n))
+        .map(|n| get(&maps, "fertilizer-to-water", n))
+        .map(|n| get(&maps, "water-to-light", n))
+        .map(|n| get(&maps, "light-to-temperature", n))
+        .map(|n| get(&maps, "temperature-to-humidity", n))
+        .map(|n| get(&maps, "humidity-to-location", n))
+        .min()
+        .unwrap()
 }
 
 fn main() {
