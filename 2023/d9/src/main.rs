39f9e2b7592f410d991432c7b516d631d9a75fa6
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-01-15 19:17:33 +0530

    done with d9
    WIP

---
+++ 2023/d9/src/main.rs
@@ -0,0 +1,55 @@
+use std::env;
+use std::fs;
+
+fn parse(input: &str) -> Vec<Vec<isize>> {
+    input
+        .lines()
+        .map(|l| l.split_whitespace().map(|n| n.parse().unwrap()).collect())
+        .collect()
+}
+
+fn predict(history: &Vec<isize>) -> (isize, isize) {
+    let mut step = history.to_owned();
+
+    let mut next = 0;
+    let mut prev = 0;
+    let mut sign = true;
+
+    while !step.iter().all(|v| *v == 0) {
+        next += step.last().unwrap();
+
+        prev += step.first().unwrap() * if sign { 1 } else { -1 };
+        sign = !sign;
+
+        step = step.windows(2).map(|w| w[1] - w[0]).collect();
+    }
+
+    (prev, next)
+}
+
+fn p1(input: &str) -> isize {
+    let histories = parse(input);
+
+    histories.iter().map(predict).map(|(_, n)| n).sum()
+}
+
+fn p2(input: &str) -> isize {
+    let histories = parse(input);
+
+    histories.iter().map(predict).map(|(p, _)| p).sum()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let part = &args[1];
+    let filepath = &args[2];
+
+    let input = fs::read_to_string(filepath).unwrap();
+
+    match part.as_str() {
+        "p1" => println!("{}", p1(&input)),
+        "p2" => println!("{}", p2(&input)),
+        _ => panic!("Invalid part"),
+    };
+}
