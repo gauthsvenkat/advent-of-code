c398bdb6b5e77b7fe08366992305f96fafd77ca1
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-12-03 09:43:17 +0100

    feat: d3 done

---
+++ 2025/d03/src/main.rs
@@ -0,0 +1,66 @@
+use std::env;
+use std::fs;
+
+fn parse(input: &str) -> Vec<Vec<usize>> {
+    input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect()
+        })
+        .collect()
+}
+
+fn get_joltage(bank: &[usize], num_batteries: usize) -> Option<usize> {
+    if num_batteries == 0 {
+        return Some(0);
+    }
+
+    if num_batteries > bank.len() {
+        return None;
+    }
+
+    let (idx, max) = bank[..=bank.len() - num_batteries]
+        .iter()
+        .enumerate()
+        .max_by(|(i, &a), (j, &b)| a.cmp(&b).then(j.cmp(i)))
+        .unwrap();
+
+    if let Some(joltage) = get_joltage(&bank[idx + 1..], num_batteries - 1) {
+        Some(max * 10_usize.pow(num_batteries as u32 - 1) + joltage)
+    } else {
+        None
+    }
+}
+
+fn p1(input: &str) -> usize {
+    let parsed_input = parse(input);
+    parsed_input
+        .iter()
+        .map(|bank| get_joltage(bank, 2).unwrap())
+        .sum()
+}
+
+fn p2(input: &str) -> usize {
+    let parsed_input = parse(input);
+    parsed_input
+        .iter()
+        .map(|bank| get_joltage(bank, 12).unwrap())
+        .sum()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let part = &args[1];
+    let filepath = &args[2];
+
+    let input = fs::read_to_string(filepath).unwrap();
+
+    match part.as_str() {
+        "p1" => println!("{}", p1(&input)),
+        "p2" => println!("{}", p2(&input)),
+        _ => panic!("Invalid part"),
+    };
+}
