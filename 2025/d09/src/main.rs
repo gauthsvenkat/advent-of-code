570330df37aa34115e18490ec30aae77afce3975
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-12-09 08:58:45 +0100

    feat: p1 done

---
+++ 2025/d09/src/main.rs
@@ -0,0 +1,66 @@
+use itertools::Itertools;
+use std::env;
+use std::fs;
+
+type Position = (usize, usize);
+
+fn parse(input: &str) -> Vec<Position> {
+    input
+        .lines()
+        .map(|l| {
+            let (x, y) = l.split_once(',').unwrap();
+
+            (x.parse().unwrap(), y.parse().unwrap())
+        })
+        .collect()
+}
+
+fn area((x1, y1): &Position, (x2, y2): &Position) -> usize {
+    (x1.abs_diff(*x2) + 1) * (y1.abs_diff(*y2) + 1)
+}
+
+fn p1(input: &str) -> usize {
+    parse(input)
+        .iter()
+        .tuple_combinations()
+        .map(|(r1, r2)| area(r1, r2))
+        .max()
+        .unwrap()
+}
+
+fn p2(input: &str) -> usize {
+    let parsed_input = parse(input);
+    todo!()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let part = &args[1];
+    let filepath = &args[2];
+
+    let input = fs::read_to_string(filepath).unwrap();
+
+    match part.as_str() {
+        "p1" => println!("{}", p1(&input)),
+        "p2" => println!("{}", p2(&input)),
+        _ => panic!("Invalid part"),
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../eg1.txt");
+
+    #[test]
+    fn test_p1_example() {
+        assert_eq!(p1(EXAMPLE), 50);
+    }
+
+    #[test]
+    fn test_p2_example() {
+        assert_eq!(p2(EXAMPLE), 24);
+    }
+}
